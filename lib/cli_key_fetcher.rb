3e07ac2f4518079bc5b5f68dbf33b3addc88c6a5
Steve Vanspall <steve@billcap.com>
2017-12-14 17:20:56 +1100

    SV: added ability to set env key to look for key in

--- lib/cli_key_fetcher.rb
+++ lib/cli_key_fetcher.rb
@@ -2,9 +2,14 @@ require 'active_support/all'
 require 'io/console'
 
 class CLIKeyFetcher
-  def initialize(cli_description = "API Key", secret_key_required = false)
+  def initialize(cli_description = "API Key", secret_key_required = false, env_key = nil)
     @secret_key_required = secret_key_required
     @cli_description = cli_description
+
+    if(env_key && ENV[env_key])
+      @key = ENV[env_key]
+      split_key_and_secret
+    end
   end
 
   def key
@@ -19,19 +24,23 @@ class CLIKeyFetcher
 
   private
 
-  def fetch_keys_if_nil
-    while(@key.blank?)
-      STDOUT.puts "#{cli_description} cannot be blank" unless @key.nil?
-      STDOUT.puts cli_description
-      @key = STDIN.noecho(&:gets).strip
-    end
-
+  def split_key_and_secret
     if(@key.match(/,/) && secret_key_required)
       split = @key.split(/,/)
 
       @key = split[0]
       @secret_key = split[1]
     end
+  end
+
+  def fetch_keys_if_nil
+    while(@key.blank?)
+      STDOUT.puts "#{cli_description} cannot be blank" unless @key.nil?
+      STDOUT.puts "#{cli_description}:"
+      @key = STDIN.noecho(&:gets).strip
+    end
+
+    split_key_and_secret
 
     while(@secret_key_required && @secret_key.blank?)
       STDOUT.puts "#{cli_description} secret cannot be blank" unless @secret_key.nil?
